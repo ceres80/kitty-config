5eb894fbf8dc2d8b15179472babb14e882bf1308
ceres80 <163446894+ceres80@users.noreply.github.com>
2024-04-27 14:02:29 +0530

    Update automated.sh

--- automated.sh
+++ automated.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 wget https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/fonts/ttf/JetBrainsMono-Regular.ttf -P ~/.local/share/fonts
 fc-cache -f -v
 mv ~/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf.old
